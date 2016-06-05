24c8bc65bbda7aa5d0594ca1a67f45eb7724395f
Cristofer [H] <cristofer.dev@gmail.com>
2016-06-04 23:47:09 -0400

    Habilitado /api/authenticate 	modified:   server.js

--- server.js
+++ server.js
@@ -58,6 +58,43 @@ apiRoutes.get('/users', function(req, res) {
 // apply the routes to our application with the prefix /api
 app.use('/api', apiRoutes);
 
+// route to authenticate a user (POST http://localhost:8080/api/authenticate)
+apiRoutes.post('/authenticate', function(req, res) {
+
+  // find the user
+  User.findOne({
+    name: req.body.name
+  }, function(err, user) {
+
+    if (err) throw err;
+
+    if (!user) {
+      res.json({ success: false, message: 'Authentication failed. User not found.' });
+    } else if (user) {
+
+      // check if password matches
+      if (user.password != req.body.password) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+
+        // if user is found and password is right
+        // create a token
+        console.log(user);
+        var token = jwt.sign(user, app.get('superSecret'));
+
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }   
+
+    }
+
+  });
+});
+
 // =======================
 // start the server ======
 // =======================
