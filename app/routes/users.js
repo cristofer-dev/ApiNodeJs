cc227b88887ce4b4b8b9a409203fb76aa7365666
César Encina Leon <ceel2004@hotmail.com>
2016-06-20 00:35:48 -0400

    AVANCES

--- app/routes/users.js
+++ app/routes/users.js
@@ -2,10 +2,12 @@ var express = require('express');
 var router = express.Router();
 var app = express();
 var jwt = require('jsonwebtoken');
+var rand = require("generate-key");
 
 var validateToken = require('../middlewares/tokenvalidate');
 var secretKey = require('../../config').secret;
 
+var mail = require('../helpers/mail').Mail;
 var User = require('../models/user');
 
 
@@ -57,19 +59,25 @@ router.post('/user/login', function(req, res) {
                         message: 'Authentication failed. Wrong password.'
                     });
                 } else {
+                    if (user.emailVerified) {
+                        // if user is found and password is right
+                        // create a token        
+                        var token = jwt.sign(user, secretKey, {
+                            expiresIn: "1h"
+                        });
 
-                    // if user is found and password is right
-                    // create a token        
-                    var token = jwt.sign(user, secretKey, {
-                        expiresIn: "1h"
-                    });
-
-                    // return the information including token as JSON
-                    res.json({
-                        success: true,
-                        message: 'Enjoy your token!',
-                        token: token
-                    });
+                        // return the information including token as JSON
+                        res.json({
+                            success: true,
+                            message: 'Enjoy your token!',
+                            token: token
+                        });
+                    } else {
+                        res.status(401).json({
+                            success: false,
+                            message: 'Cuenta no activada'
+                        });
+                    }
                 }
             });
         }
@@ -81,6 +89,8 @@ router.post('/user/login', function(req, res) {
 router.post('/user/signup', function(req, res) {
 
     var new_user = new User(req.body);
+    new_user.verificationToken = rand.generateKey();
+
     new_user.save(function(err, data) {
         if (err) {
             var msg = '';
@@ -95,12 +105,19 @@ router.post('/user/signup', function(req, res) {
                 message: msg
             }).end();
         } else {
-            //TODO: enviar mail para confirmar correo 
             console.log('User saved successfully');
             console.log(data);
             data.success = true;
-            res.status(200).json(data).end();
 
+            var host = req.headers.host;
+            var url = 'http://' + host + '/api/user/activation/' + new_user.verificationToken;
+            var link = `<a href="${url}">${url}</a>`;
+            var body = `Ha solicitado la creación de una cuenta en la comunidad de programadores IV Devs<br/>` +
+                `Es necesario que actives la cuenta en el siguiente vinculo ${link}`;
+            mail.send('Comunidad IV Devs', 'contacto@ivdevs.com', new_user.email, 'Activación de cuenta', body, function(err, json) {
+                console.log('Mail sent with verification token');
+                res.status(200).json(data).end();
+            });
         }
     });
 });
@@ -109,8 +126,38 @@ router.put('/user/', validateToken, function(req, res) {
     //TODO: editar usuario
 });
 
-router.get('user/activation/:token', function(req, res) {
+router.get('/user/activation/:token', function(req, res) {
     //TODO: activar cuenta con token
+    User.findOne({
+        verificationToken: req.params.token
+    }, function(err, user) {
+
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            if (user !== null) {
+                user.emailVerified = true;
+                user.verificationToken = null;
+                user.save(function(err, data) {
+                    if (err) res.status(500).send(err);
+                    else {
+                        console.log(data);
+                        res.send({
+                            success: true,
+                            message: "Cuenta activada"
+                        });
+                    }
+                });
+            } else {
+                res.status(500)
+                    .send({
+                        success: true,
+                        message: "TOKEN no encontrado o cuenta ya activada"
+                    });
+            }
+        }
+    });
+
 });
 
 router.post('/user/recovery/', function(req, res) {
@@ -118,7 +165,7 @@ router.post('/user/recovery/', function(req, res) {
 });
 
 // route to view a user data by token (POST http://localhost:8080/api/user/me)
-router.put('/user/me', validateToken, function(req, res) {
+router.get('/user/me', validateToken, function(req, res) {
     var user = req.decoded._doc;
 
     var filters = {
